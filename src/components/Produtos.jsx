10cbeda595ab6dc3dd0a85f4cbb8c0ee5f50d9e0
LeoScalisse <leonardo.scalisse.silva@gmail.com>
2026-09-02 08:34:25 -0300

    style: Estilizando sessão de produtos

--- src/components/Produtos.jsx
+++ src/components/Produtos.jsx
@@ -2,21 +2,22 @@ import produtos from "../data/produtos.json";
 
 function Produtos({adicionarAoCarrinho}) {
     return(
-        <section>
-            <h2>Produtos</h2>
+        <section id="sessao-produtos">
+        <h2 id="titulo-produtos">Produtos</h2>
 
+        <div className="flex-produtos">
             {produtos.map((produto) => (
-                <div key={produto.id}>
+                <div key={produto.id} className="card-produto">
                     <h3>{produto.nome}</h3>
-                    <p>R$ {produto.preco}</p>
-
-                    <button onClick={() => adicionarAoCarrinho(produto)}>
+                    <p className="preco">R$ {produto.preco}</p>
+                    <button className="btn-adicionar" onClick={() => adicionarAoCarrinho(produto)}>
                         Adicionar ao Carrinho
                     </button>
                 </div>
             ))}
-        </section>
-    );
+        </div>
+    </section>
+)
 }
 
 export default Produtos;
